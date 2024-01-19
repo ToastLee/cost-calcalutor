83288b4d7e9eaa9f3342efdd328d63fc0a63d6fe
Isaac Lee <dldltkr970@gmail.com>
2024-01-19 11:11:25 +0900

    드래그 앤 드랍 기능 React.memo를 이용하여 컴포넌트 렌더링 최적화 useCallbakc을 이용한 함수 최적화 useMemo를 이용한 결과 값 최적화 할 일 모두 지우기 할 일 목록 수정 기능 LocalStorage에 값을 저장하고 로드하는 기능을 추가했습니다.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React, {useState} from "react";
+import React, {useState, useCallback} from "react";
 import "./App.css";
-import List from "./components/List";
+import Lists from "./components/Lists";
 import Form from "./components/Form";
 
-export default function App() {
+//로컬데이터에 존재하면 가져오고 없으면 빈 공간으로 가져오기
+const initialTodoData = localStorage.getItem("todoData")
+  ? JSON.parse(localStorage.getItem("todoData"))
+  : [];
+
+function App() {
   //state로 만들기
-  const [todoData, setTodoData] = useState([]);
+  const [todoData, setTodoData] = useState(initialTodoData);
   const [value, setValue] = useState("");
+
+  //클릭한 id만 제거하고 나머지는 유지
+  //useCallback todoData가 변경시에만 다시 생성하게 만듦
+  const handleClick = useCallback((id) => {
+    let newTodoData = todoData.filter((data) => data.id !==id)
+    //State을 이용해 삭제
+    console.log("newTodoData", newTodoData);
+    setTodoData(newTodoData);
+    localStorage.setItem('todoData', JSON.stringify(newTodoData));
+    },
+    [todoData]
+  );
 
   const handleSubmit = (e) => {
     //form 안에 input 전송시 페이지 리로드 막기
@@ -22,16 +39,24 @@ export default function App() {
   // 원래 있던 할일에 새로운 할 일 추가하기(전개 연산자로 더해줌), 입력란에 글씨 지워주기
   //전 todo 데이터를 넣어주고 새로 투두 데이터를 추가 
   setTodoData((prev) => [...prev, newTodo]);
+  localStorage.setItem('todoData', JSON.stringify([...todoData, newTodo]));
   setValue("");
   };
+
+  //할 일 목록 모두 지우기 
+  const handleRemoveClick = () => {
+    setTodoData([]);
+    localStorage.setItem('todoData', JSON.stringify([]));
+  }
 
   return (
     <div className="flex items-center justify-center w-screen bg-blue-100">
       <div  className="w-full p-6 m-4 bg-white rounded-shadow md:w-3/4 md:max-w-lg lg:max-w-lg">
         <div className="flex justify-between mb-3">
           <h1>할 일 목록</h1>
+          <button onClick={handleRemoveClick}>Delete All</button>
         </div>
-        <List todoData={todoData} setTodoData={setTodoData}/>
+        <Lists handleClick={handleClick} todoData={todoData} setTodoData={setTodoData}/>
         
         <Form handleSubmit={handleSubmit} value={value} setValue={setValue} />
         
@@ -39,3 +64,5 @@ export default function App() {
     </div>
   );
 }
+
+export default App;
--- src/components/List.js
+++ src/components/List.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, {useState} from "react";
 
-export default function List({todoData, setTodoData}) {
+const List = React.memo(({
+  id, 
+  title, 
+  completed, 
+  todoData, 
+  setTodoData, 
+  provided, 
+  snapshot,
+  handleClick,
+}) => {
+  // 다른 UI제공을 위한 State 생성
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedTitle, setEditedTitle] = useState(title);
 
-
-    //체크 박스 클릭해서 완료상태로 바꾸기
+  //체크 박스 클릭해서 완료상태로 바꾸기
   const handleCompleteChange = (id) => {
     let newTodoData = todoData.map((data) => {
       //클릭한 것과 그 state안에 있는 것 중 하나가 같다면 반대 속성으로 토글
@@ -12,38 +23,97 @@ export default function List({todoData, setTodoData}) {
       }
       return data;
     });
-
     setTodoData(newTodoData);
+    localStorage.setItem('todoData', JSON.stringify(newTodoData));
   };
-
-  //클릭한 id만 제거하고 나머지는 유지
-  const handleClick = (id) => {
-    let newTodoData = todoData.filter((data) => data.id !==id)
-    //State을 이용해 삭제
-    console.log("newTodoData", newTodoData);
-    setTodoData(newTodoData);
-  };
-
-  return (
-    <div>
-      {todoData.map((data) => (
-        <div key={data.id}>
-          <div className='flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded'>
-            <div>
-              <input 
-                type="checkbox"
-                //체크박스에 변화가 생기면 data.id를 콜하기
-                onChange={() => handleCompleteChange(data.id)} 
-                defaultChecked={data.completed} 
-              />{" "}
-              <span className={data.completed ? "line-through" : undefined}>{data.title}</span>
-            </div>
-            <div className="items-center">
-              <button className='px-4 py-2 float-right' onClick={() => handleClick(data.id)}>x</button>
-            </div>
+  
+  const handleEditChange = (event) => {
+    setEditedTitle(event.target.value);
+  }
+  
+  const handleSubmit = (event) => {
+    //폼이 제출되면 페이지 새로고침 막기
+    event.preventDefault();
+    
+    //새로운 배열로 변경하기
+      let newTodoData = todoData.map((data)=> {
+        //만약 현재 요소의 id가 어떤 변수 id와 일치할 경우 title 값을 editedTitle로
+        if(data.id === id) {
+          data.title = editedTitle;
+        }
+        //바뀐 값을 전달해주고
+        return data;
+      });
+      //바뀐 값을 투두 데이터에 저장하고, 수정상태를 불가로 변경
+      setTodoData(newTodoData);
+      localStorage.setItem('todoData', JSON.stringify(newTodoData));
+      setIsEditing(false);
+  }
+  
+  if(isEditing) {
+    return(
+      <div className={`flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 border rounded`}>  
+        <div className='items-center'>
+          <form onSubmit={handleSubmit}>
+            <input 
+              value={editedTitle}
+              onChange={handleEditChange}
+              className="w-full px-3 py-2 mr-4 text-gray-500 rounded" 
+            />
+          </form>
+        </div>
+          <div className="items-center">
+            <button className='px-4 py-2 float-right' 
+              onClick={() => setIsEditing(false)}>
+                x
+            </button>
+              {/* 수정하기 버튼 추가와 클릭시 수정 상태로 State 변경 */}
+            <button
+              onClick={handleSubmit}
+              className='px-4 py-2 float-right'
+              type="submit"
+            >
+              save
+            </button>
+        </div>
+      </div>
+    );
+  } else {
+  // 조건에 따른 UI렌더링 
+      return (
+        <div key={id} 
+          {...provided.draggableProps} 
+          ref={provided.innerRef} {...provided.dragHandleProps}
+          className={`${snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"}
+          flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 border rounded`}
+        >  
+      <div className='items-center'>
+            <input 
+              type="checkbox"
+              //체크박스에 변화가 생기면 data.id를 콜하기
+              onChange={() => handleCompleteChange(id)} 
+              defaultChecked={completed} 
+            />{" "}
+            <span 
+            className={completed ? "line-through" : undefined}>
+              {title}
+            </span>
+      </div>
+          <div className="items-center">
+            <button className='px-4 py-2 float-right' 
+                onClick={() => handleClick(id)}>
+                x
+            </button>
+            {/* 수정하기 버튼 추가와 클릭시 수정 상태로 State 변경 */}
+            <button className='px-4 py-2 float-right' 
+                onClick={() => setIsEditing(true)}>
+                edit
+            </button>
           </div>
         </div>
-      ))}
-    </div>
-  );
-}
+      );
+    }
+  }
+);
+//Lists로 연결하기
+export default List;
